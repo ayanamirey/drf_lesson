09fe79fb8c9817f181e0a2ec4ab6c7a97111d80d
Sanjar <sanjar.ravshanbekov@gmail.com>
2022-05-01 17:56:51 +0500

    Введение в сериализацию. Класс Serializer

--- women/serializers.py
+++ women/serializers.py
@@ -1,16 +1,38 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from women.models import Women, Category
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ('name',)
+# class WomenModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
 
 
-class WomenSerializer(serializers.ModelSerializer):
-    # category = CategorySerializer()
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    category_id = serializers.IntegerField()
 
-    class Meta:
-        model = Women
-        fields = ('title', 'category_id')
+
+# def encode():
+#     model = WomenModel("Angelina Jolie", "Content: Angelina Jolie")
+#     model_sr = WomenSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+#
+#
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Angelina Jolie","content":"Angelina Jolie"}')
+#     data = JSONParser().parse(stream)
+#     serializer = WomenSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)  #validated_data и есть результат декодирования jsons строки

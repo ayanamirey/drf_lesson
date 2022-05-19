aac30588c91196b0ed8a00ca55318c777784bda1
Sanjar <sanjar.ravshanbekov@gmail.com>
2022-05-19 23:49:54 +0500

    Swagger, redoc

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -3,9 +3,23 @@ from django.urls import path, include, re_path
 from women.views import WomenAPIList, WomenAPIUpdate, WomenAPIDestroy
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+# from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-# router = routers.DefaultRouter()
-# router.register(r'women', WomenViewSet, basename='women')
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Women API',
+        description='Oddiy project APIsi',
+        default_version='v1',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='sanjar.ravshanbekov@gmail.com'),
+        license=openapi.License(name="Women project"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,7 +32,9 @@ urlpatterns = [
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('swagger/', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0), name='schema-redoc')
     # path('api/v1/', include(router.urls))
 ]
-
-
